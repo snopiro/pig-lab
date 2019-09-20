fa1ed7a272ea4142417ae129891f7e616633f614
Chris Khoo <khoochris13@gmail.com>
2019-09-20 09:31:48 -0600

    stage

--- Pig Lab/src/Person.java
+++ Pig Lab/src/Person.java
@@ -5,13 +5,14 @@ public class Person {
 	//instance variables
 	private String name;
 	private int score;
+	private int fakescore;
 	
 	//initializing scanner
 	Scanner sc = new Scanner(System.in);
 	
 	//constructor
-	public Person(String _name) {
-		name = _name;
+	public Person() {
+		name = sc.next();
 		score = 0;
 	}	
 	
@@ -26,14 +27,20 @@ public class Person {
 	
 	//roll method
 	public void roll(boolean isTrue) {
+		
+		fakescore = score;
+		
 		if(isTrue) {
 			int dice = (int) Math.random() * 5 + 1;
 			if(dice>1) {
-				score += dice;	
+				fakescore += dice;	
 			}
 			else
 				bust();
 				
+		}
+		else {
+			score = fakescore;
 		}
 	}
 	
